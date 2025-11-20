0439d90a9bb9232571835c9958d69aea6537f1de
sohelr-dev <sohe.4273@gmail.com>
2025-11-20 10:04:06 +0600

    laravel jwt inveromant setup

---
+++ lara-jwt-app/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+   public function register(Request $request){
+        $request->validate([
+            'name'=>'required|max:255',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|confirmed',
+        ]);
+        $user =User::create($request->all());
+        return response()->json([
+            'success'=>true,
+            'massage'=>'Registion SuccessFull .',
+            'data'=>$user
+        ],201);
+    }
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        $user = User::where('email',$request->email)->first();
+        if(!$user){
+            return response()->json([
+                'error'=>true,
+                'message'=>'Email Does not match!',
+            ],401);     
+        }else{
+            if(Hash::check($request->password,$user->password)){
+                $token =JWTAuth::fromUser($user);       //fromUser
+                return response()->json([
+                    'success'=>true,
+                    'message'=>'login SuccessFull.',
+                    'data'=>$user,
+                    'token'=>$token,
+                ],201);
+            }else{
+                return response()->json([
+                    'error'=>true,
+                    'message'=>'Password Does Not Match',
+                ],401);
+            }
+        }
+    }
+    public function logout(){
+        
+        return response()->json([
+            'success'=>true,
+            'message'=>'logout SuccessFull',
+        ]);
+    }
+}
---
+++ lara-jwt-app/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test',function(){
+    return "'hello jewt'";
+});
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
